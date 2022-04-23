5378fc7be0da76a2b0a159ba258232fc9bbf25e0
lambpati <lambpati@isu.edu>
2022-04-22 21:20:14 -0600

    Getting closer to solution

--- 907C/shockers_answer.cpp
+++ 907C/shockers_answer.cpp
@@ -10,6 +10,7 @@
 #include <fstream>
 
 int sum = 26;
+int count = 0;
 
 std::vector<bool> findPath(std::string &word){
     std::vector<bool> locations(26, 0);
@@ -18,7 +19,6 @@ std::vector<bool> findPath(std::string &word){
     }
     return locations;
 }
-
 std::vector<bool> noShock(std::vector<bool> &location, std::vector<bool> &valid){
     for(int letter = 0; letter < 26; letter++){
         if(valid[letter] && location[letter]){
@@ -48,51 +48,53 @@ std::vector<bool> guess(std::string &word, std::vector<bool> &valid){
     return valid;
 }
 
-std::string determineAction(std::string &str){
+int determineAction(std::string &str){
     std::string word;
-    int count;
     std::vector<bool> valid(26,1);
     word = std::regex_replace(str, std::regex("[!.? ]"), "$2");
     std::vector<bool> loc = findPath(word);
-    if(std::regex_match(str, std::regex("!"))){
+    if(str.find('!') != std::string::npos){
+        std::cout << "! activated" << std::endl; 
         shock(loc, valid);
     }
-    else if(std::regex_match(str, std::regex("."))){
+    else if(str.find('.') != std::string::npos){
+        std::cout << ". activated" << std::endl;
         noShock(loc, valid);
         count += (sum == 1);
     }
-    else if(std::regex_match(str, std::regex("?"))){
+    else if(str.find('?') != std::string::npos){
+        std::cout << "? activated" << std::endl;
         guess(word, valid);
     }
-    return "No action found.";
+    return count;
 }
 
 int main(int argc, char const *argv[]) {
     std::string filename;
     std::string str;
     int lineNum = 0;
-    std::cout << "Please specify file to use for shockers (including extension & relative path)" << std::endl;
+    std::cout << "Please specify file to convert (including extension & relative path)" << std::endl;
     std::cin >> filename;
-    std::ifstream response;
-    response.exceptions(std::ifstream::badbit); //I dont see why to check for badbits
-        try{
-            // File read and write
-            response.open(filename);
-            if( response.is_open()){
-                throw std::ifstream::failure("Cannot find file");
-            }
-            std::getline(response,str);
-            // Assuming first line is always the amount of times to iterate
-                lineNum = std::stoi(str);
-
-            for(int i = 0; i < lineNum; i++){
-                std::getline(response, str);
-                std::cout << determineAction(str) << std::endl;
-            }
-
+    std::ifstream btest;
+    btest.exceptions(std::ifstream::badbit); //I dont see why to check for badbits
+    try{
+        // File read and write
+        btest.open(filename);
+        if(!btest.is_open()){
+        throw std::ifstream::failure("Cannot find file");
         }
-        catch(const std::ifstream::failure& e){
-            std::cerr << e.what() << std::endl;
+        std::getline(btest,str);
+        // Assuming first line is always the amount of times to iterate
+        lineNum = std::stoi(str);
+
+        for(int i = 0; i < lineNum; i++){
+            std::getline(btest, str);
+            std::cout << determineAction(str) << std::endl;
         }
+
+    }
+    catch(const std::ifstream::failure& e){
+        std::cerr << e.what() << std::endl;
+    }
     return 0;
 }
